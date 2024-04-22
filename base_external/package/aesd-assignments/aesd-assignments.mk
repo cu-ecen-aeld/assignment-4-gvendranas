1b9642797b611567bc5324a090124669f40c75ce
gvendranas <gilles.vendranas@gmail.com>
2024-04-22 23:17:42 +0200

    Performing a test with a third party SW

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 80b08fa58b228bf59d8b00486fb02179ad7c83bd
+AESD_ASSIGNMENTS_VERSION = da6b933d2dc66328457feb58705538b4bcfb4411
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
